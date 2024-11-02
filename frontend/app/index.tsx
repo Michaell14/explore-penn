89e05d14b4abb39fbe41ee996e9987e26f77d328
Eric Lee <ericslee1207@gmail.com>
2024-11-02 15:31:20 -0400

    auth

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,25 @@
 // app/index.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { useAuth } from '../hooks/useAuth';
+import { useRouter } from 'expo-router';
 
 const Index = () => {
+  const {signOut, user} = useAuth()
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut().then(() => router.replace('/auth/sign-in'));
+    } catch (error) {
+      console.error("Sign out error:", error);
+    }
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Home Screen</Text>
+      <Text>Welcome {user?.displayName}</Text>
+      <Button title="Log Out" onPress={handleSignOut} />
     </View>
   );
 };
