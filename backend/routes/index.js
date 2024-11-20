37d1f4c88295eb372eb048c3ae3dfa47ef13c408
kyue26 <kyue26@gmail.com>
2024-11-20 18:42:34 -0500

    organizations

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import userRoutes from './user.routes.js'
-// import socialPinsRoutes from './socialPin.routes.js';
+import orgRoutes from './organization.routes.js';
 import eventPinsRoutes from './eventPin.routes.js';
 import { notify } from '../controller/notif.controller.js'
 import locRoutes from './locations.routes.js'
@@ -8,7 +8,7 @@ import locRoutes from './locations.routes.js'
 const router = express.Router();
 
 router.use('/users', userRoutes);
-// router.use('/api/socialPins', socialPinsRoutes);
+router.use('/org', orgRoutes);
 router.use('/eventPins', eventPinsRoutes);
 router.post('/notify', notify);
 router.use('/loc', locRoutes);
